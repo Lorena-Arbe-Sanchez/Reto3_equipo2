284b50382b542b5239f4e16980edbf8d33e949ea
Lorena <lorena.arbe@ikasle.egibide.org>
2025-02-12 23:23:26 +0100

    Añadir seeders de admins para que funcionen los de las actividades.

--- reto3_equipo2/database/seeders/AdministradoresSeeder.php
+++ reto3_equipo2/database/seeders/AdministradoresSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class AdministradoresSeeder extends Seeder
 {
@@ -12,6 +13,29 @@ class AdministradoresSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('administradores')->insert([
+            [
+                'nombre' => 'Carlos',
+                'apellidos' => 'Fernández López',
+                'dni' => '12345678Z',
+                'direccion' => 'Calle Mayor, 10',
+                'codigo_postal' => '28001',
+                'usuario' => 'carlosf',
+                'password' => bcrypt('contra123'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nombre' => 'María',
+                'apellidos' => 'García Pérez',
+                'dni' => '87654321X',
+                'direccion' => 'Avenida de la Paz, 5',
+                'codigo_postal' => '28002',
+                'usuario' => 'mariag',
+                'password' => bcrypt('contra123'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
     }
 }
