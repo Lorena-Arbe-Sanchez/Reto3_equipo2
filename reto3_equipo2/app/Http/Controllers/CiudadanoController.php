96533ae4fa8cb24a1c99978dacc48b22a24e13e4
JulenCorpas2004 <julen.corpas@ikasle.egibide.org>
2025-02-17 09:54:41 +0100

    Crear ciudadanos hecho

--- reto3_equipo2/app/Http/Controllers/CiudadanoController.php
+++ reto3_equipo2/app/Http/Controllers/CiudadanoController.php
@@ -12,4 +12,37 @@ class CiudadanoController extends Controller
         $ciudadanos = Ciudadano::all();
         return view('ciudadano.createCiudadano', compact('ciudadanos'));
     }
+
+    public function store(Request $request){
+        try {
+            $validator = Validator::make($request->all(), [
+                'nombre' => 'required|string',
+                'apellidos' => 'required|string',
+                'dni' => 'required|string',
+                'direccion' => 'required|string',
+                'codigo_postal' => 'required|string',
+                'juego_barcos' => 'required|string',
+            ]);
+
+            if ($validator->fails()) {
+                return redirect()->back()->withErrors($validator)->withInput();
+            }
+
+            $ciudadano = new Ciudadano();
+
+            $ciudadano->nombre = $request->nombre;
+            $ciudadano->apellidos = $request->apellidos;
+            $ciudadano->dni = $request->dni;
+            $ciudadano->direccion = $request->direccion;
+            $ciudadano->codigo_postal = $request->codigo_postal;
+            $ciudadano->juego_barcos = $request->juego_barcos;
+
+            $ciudadano->save();
+        } catch (\Exception $exception) {
+            return redirect()->back()->withErrors(['error' => $exception->getMessage()])->withInput();
+        }
+
+        return view('ciudadano.createCiudadano', compact('ciudadano'));
+
+    }
 }
